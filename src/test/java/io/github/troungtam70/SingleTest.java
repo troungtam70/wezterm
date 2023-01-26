58c21f1bfb60f875715f98d00eb95a915c288ee3
Troung Tam <troungtam70@proton.me>
2023-01-27 02:26:31 +0700

    ...

---
+++ src/test/java/io/github/troungtam70/SingleTest.java
@@ -0,0 +1,11 @@
+package io.github.troungtam70;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class SingleTest {
+  @Test
+  void test1() {
+    assertEquals(2, 1 + 1);
+  }
+}
